2efa62d2b32e977fe91e9ee78bd5a3a0cce4987e
Oliver Vik <olivik007@gmail.com>
2025-01-09 12:23:14 +0100

    Initial commit
    making a function to generate random users to insert into database

---
+++ addElever.js
@@ -0,0 +1,22 @@
+import fakerNB_NO from '@faker-js/faker';
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const db = mysql.createPool({
+    host: process.env.sqlHost,
+    user: process.env.sqlUser,
+    password: process.env.sqlPass,
+    database: process.env.sqlDb,
+}).promise()
+
+let fornavn = fakerNB_NO.person.firstName();
+let etternavn = fakerNB_NO.person.lastName();
+
+let navn = fornavn + " " + etternavn;
+console.log(navn);
+
+function elever (fornavn, etternavn) {
+    
+}
