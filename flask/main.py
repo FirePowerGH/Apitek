4e75edcd075fc4c374b8d66480f919e68508229c
Oliver Vik <olivik007@gmail.com>
2025-01-13 15:17:44 +0100

    flaskify

---
+++ flask/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+import mysql.connector
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+sqlConfig = {
+    "host": getenv("sqlHost"),
+    "user": getenv("sqlUser"),
+    "password": getenv("sqlPass"),
+    "database": getenv("sqlDb")
+}
+
+app = Flask(__name__)
+
+@app.route("/elev/<elevID>")
+def index(elevID):
+    try:
+        db = mysql.connector.connect(**sqlConfig)
+        cursor = db.cursor()
+
+        query = "SELECT * FROM elever WHERE id = %s;"
+    
+        cursor.execute(query, (elevID, ))
+        data = cursor.fetchall()
+    except mysql.connector.Error as e:
+        db = None
+        return(f"Error: {e}")
+    finally:
+        if db != None and db.is_connected():
+            cursor.close()
+            db.close()
+    return data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
