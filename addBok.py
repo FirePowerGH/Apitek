55c9b104bd00f870b9e1d1ebd3f99231f7b16605
Oliver Vik <olivik007@gmail.com>
2025-01-16 08:54:41 +0100

    insertIntoDb() processes data

--- addBok.py
+++ addBok.py
@@ -2,7 +2,9 @@ import requests
 import mysql.connector
 from dotenv import load_dotenv
 from os import getenv
+from random import randint, choice
 import re
+import string
 
 load_dotenv()
 
@@ -22,10 +24,8 @@ def getBook():
 
     response = requests.get(f"https://openlibrary.org/search.json?title={query}&fields=title,author_name&lang=en&limit=1")
     if response.status_code == 200:
-        print(response.json(), "\n")
-        tittel = response.json()["docs"][0]["title"]
-        forf = response.json()["docs"][0]["author_name"][0]
-        print(f"{tittel} er laget av {forf}")
+        data = response.json()
+        insertIntoDb(data)
     else:
         print(f"Noe gikk galt: {response.status_code}")
 
@@ -33,6 +33,22 @@ def insertIntoDb(data):
     try:
         db = mysql.connector.connect(**sqlConfig)
         cursor = db.cursor()
+
+        tittel = data["docs"][0]["title"]
+        forf = data["docs"][0]["author_name"][0]
+
+        tall = randint(1,32)
+
+        if tall < 10:
+            tall = "0" + str(tall)
+        
+        bokstav = choice(string.ascii_uppercase)
+
+        hylle = str(tall) + bokstav
+
+        print(f"Navn: {tittel} \nForfatter: {forf} \nHylle: {hylle}")
+
+        query = "INSERT INTO boker"
     except mysql.connector.Error as e:
         db = None
         return(f"Error: {e}")
