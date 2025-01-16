40961dca7709edbd4664a2d2ac1edf270ec3908b
FirePower <31512975+FirePowerGH@users.noreply.github.com>
2025-01-16 14:22:26 +0100

    Gjorde det mulig å jobbe på maccen

--- flask/main.py
+++ flask/main.py
@@ -31,4 +31,4 @@ def utlan():
     return render_template("utlan.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
---
+++ flask/static/script.js
@@ -0,0 +1,3 @@
+function toggleTable() {
+    console.log("sweetch")
+}
